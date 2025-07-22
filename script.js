62811b1c846e7600fafdfc17bfd274b29ad65382
Ntombi <ntomblinah@gmail.com>
2025-07-23 00:56:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 window.addEventListener("scroll", function () {
-  const reveals = document.querySelectorAll("section, .project-card, .badge, footer");
-  for (let i = 0; i < reveals.length; i++) {
+  const elements = document.querySelectorAll(
+    "section, .project-card, .badge, footer"
+  );
+
+  for (let i = 0; i < elements.length; i++) {
     const windowHeight = window.innerHeight;
-    const revealTop = reveals[i].getBoundingClientRect().top;
-    if (revealTop < windowHeight - 150) {
-      reveals[i].classList.add("visible");
+    const elementTop = elements[i].getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (elementTop < windowHeight - revealPoint) {
+      elements[i].classList.add("visible");
     }
   }
 });
