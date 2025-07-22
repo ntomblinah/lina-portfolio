5a28b5ab77349e085f64270b206522b833ae1d40
Ntombi <ntomblinah@gmail.com>
2025-07-22 21:55:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+  const header = document.querySelector('header');
+  header.style.opacity = '0';
+  header.style.transition = 'opacity 1.5s ease-in-out';
+  setTimeout(() => {
+    header.style.opacity = '1';
+  }, 100);
+});
